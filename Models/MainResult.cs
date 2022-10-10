60a50e31f09a6ebea468756abdfb0f06cc8cf85b
AlegDreg <80702802+AlegDreg@users.noreply.github.com>
2022-10-10 23:34:31 +0300

    Add files via upload

--- Models/MainResult.cs
+++ Models/MainResult.cs
@@ -1,4 +1,5 @@
-namespace YaDiskSdk.Models
+<<<<<<< HEAD
+﻿namespace YaDiskSdk.Models
 {
     public class MainResult<T>
     {
@@ -12,4 +13,20 @@ namespace YaDiskSdk.Models
         public string method { get; set; }
         public bool templated { get; set; }
     }
+=======
+﻿namespace YaDiskSdk.Models
+{
+    public class MainResult : IResult<Res>
+    {
+        public System.Exception exception { get; set; }
+        public Res result { get; set; }
+    }
+
+    public class Res
+    {
+        public string href { get; set; }
+        public string method { get; set; }
+        public bool templated { get; set; }
+    }
+>>>>>>> f726f92d39c1a01454602106d5e7026661d8f9a9
 }
