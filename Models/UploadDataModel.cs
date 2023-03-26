6c4e8c40a978a26425299a4072a3b1c8289ee0e0
AlegDreg <80702802+AlegDreg@users.noreply.github.com>
2023-03-26 23:21:47 +0300

    Update UploadDataModel.cs

--- Models/UploadDataModel.cs
+++ Models/UploadDataModel.cs
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-﻿namespace YaDiskSdk.Models
+namespace YaDiskSdk.Models
 {
     public class UploadDataModel
     {
         public string fileUrl { get; set; }
         public string newFileUrl { get; set; }
     }
-=======
-﻿namespace YaDiskSdk.Models
-{
-    public class UploadDataModel
-    {
-        public string fileUrl { get; set; }
-        public string newFileUrl { get; set; }
-    }
->>>>>>> f726f92d39c1a01454602106d5e7026661d8f9a9
 }
