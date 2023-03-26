f5385b30aefb73a50bbc78a3b9949741f2777dac
AlegDreg <80702802+AlegDreg@users.noreply.github.com>
2023-03-26 23:20:46 +0300

    Update RemoveDataModel.cs

--- Models/RemoveDataModel.cs
+++ Models/RemoveDataModel.cs
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-﻿namespace YaDiskSdk.Models
+namespace YaDiskSdk.Models
 {
     public class RemoveDataModel
     {
         public string path { get; set; }
         public bool permanently { get; set; } = true;
     }
-=======
-﻿namespace YaDiskSdk.Models
-{
-    public class RemoveDataModel
-    {
-        public string path { get; set; }
-        public bool permanently { get; set; } = true;
-    }
->>>>>>> f726f92d39c1a01454602106d5e7026661d8f9a9
 }
