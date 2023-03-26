547fdf5c471a8f8ab7b907abbbc4318eeb1220f7
AlegDreg <80702802+AlegDreg@users.noreply.github.com>
2023-03-26 23:21:04 +0300

    Update BaseInfoModel.cs

--- Models/BaseInfoModel.cs
+++ Models/BaseInfoModel.cs
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-﻿namespace YaDiskSdk.Models
+namespace YaDiskSdk.Models
 {
     public class BaseInfoModel
     {
         public string url { get; set; }
         public string token { get; set; }
     }
-=======
-﻿namespace YaDiskSdk.Models
-{
-    public class BaseInfoModel
-    {
-        public string url { get; set; }
-        public string token { get; set; }
-    }
->>>>>>> f726f92d39c1a01454602106d5e7026661d8f9a9
 }
